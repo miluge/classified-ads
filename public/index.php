be9e048269d348488007f83a10ac68b3426328a8
miluge <gllmblndl@gmail.com>
2020-08-24 15:00:33 +0200

    commit before pull for db data

--- public/index.php
+++ public/index.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/tailwind.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-    <pre>
-    <?php
-    print_r($_SERVER);
-    ?>
-    </pre>
-</body>
-</html>
+<?php
+
+require_once '../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../application/template');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+// Load template
+
+$template = $twig->load('index.html.twig');
+echo $template->render();
