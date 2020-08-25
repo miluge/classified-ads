be0492e8a23ee77298128476bfe909555e260eb4
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-25 16:30:52 +0200

    add delete method

--- application/class/Manager/UserManager.php
+++ application/class/Manager/UserManager.php
@@ -6,12 +6,12 @@ use \Ads\User as User;
 class UserManager extends Database
 {
     /**
-     * @param id $id [id of user to fetch in classified-ads database]
+     * @param integer $id [id of user to fetch in database]
      * @return User|array [fetched User instance on success | ["error" => message] on fail]
      */
-    public function getUserById($id){
+    public static function getUserById($id){
         try{
-            $database = $this -> connect();
+            $database = self::connect();
             $select = "SELECT id, email, lastName, firstName, phone FROM user WHERE id = :id";
             $request = $database -> prepare($select);
             $request -> bindValue(':id', $id);
@@ -21,7 +21,27 @@ class UserManager extends Database
             } else {
                 throw new \InvalidArgumentException("User not found !");
             }
-        } catch (\InvalidArgumentException $e) {
+        } catch (\Exception $e) {
+            return(["error"=>$e->getMessage()]);
+        }
+    }
+
+    /**
+     * @param id $id [id of user to be deleted in database]
+     * @return array [["error" => false] on success | ["error" => message] on fail]
+     */
+    public static function deleteUserById($id){
+        try{
+            $database = self::connect();
+            $delete = "DELETE FROM user WHERE id = :id";
+            $request = $database -> prepare($delete);
+            $request -> bindValue(':id', $id);
+            if ($request->execute()){
+                return ["error" => false];
+            }else {
+                throw new \InvalidArgumentException("User not found !");
+            }
+        } catch (\Exception $e) {
             return(["error"=>$e->getMessage()]);
         }
     }
