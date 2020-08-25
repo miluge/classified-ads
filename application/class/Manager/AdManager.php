3696df3f087c343cd28c57f3d197e95db7963639
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-25 16:30:30 +0200

    add delete methods

--- application/class/Manager/AdManager.php
+++ application/class/Manager/AdManager.php
@@ -2,16 +2,55 @@
 namespace Ads\Manager;
 use \Ads\Database as Database;
 use \Ads\Ad as Ad;
+use \Ads\Manager\User as UserManager;
 
 class AdManager extends Database
 {
     /**
-     * @param id $id [id of ad to fetch in classified-ads database]
+     * @param integer $id [id of ad to be fetched in database]
+     * @return integer|array [user_id of ad on success | ["error" => message] on fail]
+     */
+    public static function getUser_id($id){
+        try{
+            $database = self::connect();
+            $select = "SELECT user_id FROM ad WHERE id=:id"; 
+            $request = $database -> prepare($select);
+            $request -> bindValue(':id', $id);
+            $request -> execute();
+            if ($user_id = $request->fetch()){
+                return $user_id;
+            }else{
+                throw new \InvalidArgumentException("Ad user not found !");
+            }
+        } catch (\Exception $e) {
+            return(["error"=>$e->getMessage()]);
+        }
+    }
+
+    /**
+     * @param integer $user_id [user_id to filter ads fetched in database]
+     * @return boolean|array [["user_id"=>$user_id] if exists | false if not exists | ["error" => message] on fail]
+     */
+    public static function user_idExists($user_id){
+        try{
+            $database = self::connect();
+            $select = "SELECT user_id FROM ad WHERE user_id=:user_id"; 
+            $request = $database -> prepare($select);
+            $request -> bindValue(':user_id', $user_id);
+            $request -> execute();
+            return $request->fetch();
+        } catch (\Exception $e) {
+            return(["error"=>$e->getMessage()]);
+        }
+    }
+
+    /**
+     * @param integer $id [id of ad to fetch in database]
      * @return Ad|array [fetched Ad instance on success | ["error" => message] on fail]
      */
-    public function getAdById($id){
+    public static function getAdById($id){
         try{
-            $database = $this -> connect();
+            $database = self::connect();
             $select = "SELECT id, user_id, category_id, title, description, creationDate, validationDate, picture FROM ad WHERE id = :id";
             $request = $database -> prepare($select);
             $request -> bindValue(':id', $id);
@@ -21,7 +60,7 @@ class AdManager extends Database
             } else {
                 throw new \InvalidArgumentException("Ad not found !");
             }
-        } catch (\InvalidArgumentException $e) {
+        } catch (\Exception $e) {
             return(["error"=>$e->getMessage()]);
         }
     }
@@ -29,9 +68,9 @@ class AdManager extends Database
     /**
      * @return array [array of fetched Ad instances on success | ["error" => message] on fail]
      */
-    public function getAllAds(){
+    public static function getAllAds(){
         try{
-            $database = $this -> connect();
+            $database = self::connect();
             $select = "SELECT id, user_id, category_id, title, description, creationDate, validationDate, picture FROM ad";
             $request = $database -> prepare($select);
             $request -> execute();
@@ -42,7 +81,32 @@ class AdManager extends Database
             } else {
                 throw new \LengthException("No ads found !");
             }
-        } catch (\LengthException $e) {
+        } catch (\Exception $e) {
+            return(["error"=>$e->getMessage()]);
+        }
+    }
+
+    /**
+     * @param id $id [id of ad to be deleted in database]
+     * if user have NO other ads, delete user from user table
+     * @return array [["error" => false] on success | ["error" => message] on fail]
+     */
+    public static function deleteAdById($id){
+        try{
+            $database = self::connect();
+            $user_id = self::getUser_id($id);
+            $delete = "DELETE FROM ad WHERE id = :id";
+            $request = $database -> prepare($delete);
+            $request -> bindValue(':id', $id);
+            if ($request -> execute()){
+                if (! self::user_idExists($user_id)){
+                    \UserManager::deleteUserById($user_id);
+                }
+                return ["error" => false];
+            } else {
+                throw new \InvalidArgumentException("Ad not found !");
+            }
+        } catch (\Exception $e) {
             return(["error"=>$e->getMessage()]);
         }
     }
