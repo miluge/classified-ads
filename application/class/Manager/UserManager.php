2110e6ca36242df99ba9e1850b5ea48490c360d8
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-26 10:19:16 +0200

    change getUserByEmail to getUserIdByEmail

--- application/class/Manager/UserManager.php
+++ application/class/Manager/UserManager.php
@@ -7,8 +7,8 @@ class UserManager extends Database
 {
     //METHODE NON ENCORE UTILISEE
     // /**
-    //  * @param integer $id [id of user to fetch in database]
-    //  * @return User|array [fetched User instance on success | ["error" => message] on fail]
+    //  * @param integer $id id of user to fetch in database
+    //  * @return User|array fetched User instance on success | ["error" => message] on fail
     //  */
     // public static function getUserById($id){
     //     try{
@@ -28,10 +28,10 @@ class UserManager extends Database
     // }
 
     /**
-     * @param string $email [email of user to fetch in database]
-     * @return integer [matching user id on success, 0 if user doesn't exist | ["error" => message] on fail]
+     * @param string $email email of user to fetch in database
+     * @return integer|array matching user id on success, 0 if user doesn't exist | ["error" => message] on fail
      */
-    public static function getUserByEmail($email){
+    public static function getUserIdByEmail($email){
         try{
             $pdo = self::connect();
             $select = "SELECT id FROM user WHERE email = :email";
@@ -49,8 +49,29 @@ class UserManager extends Database
     }
 
     /**
-     * @param id $id [id of user to be deleted in database]
-     * @return array [["error" => false] on success | ["error" => message] on fail]
+     * @param User $user User instance to insert in database
+     * @return integer matching user id on success, 0 if user doesn't exist | ["error" => message] on fail
+     */
+    public static function InsertUser($user){
+        try{
+            $pdo = self::connect();
+            $select = "SELECT id FROM user WHERE email = :email";
+            $request = $pdo -> prepare($select);
+            $request -> bindValue(':email', $email);
+            $request -> execute();
+            if ($user = $request->fetch()) {
+                return $user["id"];
+            } else {
+                return 0;
+            }
+        } catch (\Exception $e) {
+            return(["error"=>$e->getMessage()]);
+        }
+    }
+
+    /**
+     * @param id $id id of user to be deleted in database
+     * @return array ["error" => false] on success | ["error" => message] on fail
      */
     public static function deleteUserById($id){
         try{
