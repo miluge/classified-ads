1921ee6d9f92e2f24bb7ff04809cb27f59106b33
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-26 11:36:04 +0200

    add insertUser method

--- application/class/Manager/UserManager.php
+++ application/class/Manager/UserManager.php
@@ -5,43 +5,21 @@ use \Ads\User as User;
 
 class UserManager extends Database
 {
-    //METHODE NON ENCORE UTILISEE
-    // /**
-    //  * @param integer $id id of user to fetch in database
-    //  * @return User|array fetched User instance on success | ["error" => message] on fail
-    //  */
-    // public static function getUserById($id){
-    //     try{
-    //         $pdo = self::connect();
-    //         $select = "SELECT id, email, lastName, firstName, phone FROM user WHERE id = :id";
-    //         $request = $pdo -> prepare($select);
-    //         $request -> bindValue(':id', $id);
-    //         $request -> execute();
-    //         if ($user = $request->fetch()) {
-    //             return new User($user);
-    //         } else {
-    //             throw new \InvalidArgumentException("User not found !");
-    //         }
-    //     } catch (\Exception $e) {
-    //         return(["error"=>$e->getMessage()]);
-    //     }
-    // }
-
     /**
-     * @param string $email email of user to fetch in database
-     * @return integer|array matching user id on success, 0 if user doesn't exist | ["error" => message] on fail
+     * @param string $email email of user to select in database
+     * @return User|array selected User instance on success | ["error" => message] on fail
      */
-    public static function getUserIdByEmail($email){
+    public static function getUser($email){
         try{
             $pdo = self::connect();
-            $select = "SELECT id FROM user WHERE email = :email";
+            $select = "SELECT email, lastName, firstName, phone FROM user WHERE email = :email";
             $request = $pdo -> prepare($select);
             $request -> bindValue(':email', $email);
             $request -> execute();
             if ($user = $request->fetch()) {
-                return $user["id"];
+                return new User($user);
             } else {
-                return 0;
+                throw new \InvalidArgumentException("User not found !");
             }
         } catch (\Exception $e) {
             return(["error"=>$e->getMessage()]);
@@ -50,19 +28,39 @@ class UserManager extends Database
 
     /**
      * @param User $user User instance to insert in database
-     * @return integer matching user id on success, 0 if user doesn't exist | ["error" => message] on fail
+     * check if user already exists:
+     * insert if not
+     * update if so
+     * @return array ["error" => false] on success | ["error" => message] on fail
      */
     public static function InsertUser($user){
         try{
             $pdo = self::connect();
-            $select = "SELECT id FROM user WHERE email = :email";
-            $request = $pdo -> prepare($select);
-            $request -> bindValue(':email', $email);
-            $request -> execute();
-            if ($user = $request->fetch()) {
-                return $user["id"];
+            $previousUser = self::getUser($user->email);
+            if (is_array($previousUser)) {
+                $insert = "INSERT INTO user (email, lastName, firstName, phone) VALUES (:email, :lastName, :firstName, :phone)";
+                $request = $pdo -> prepare($insert);
+                $request -> bindValue(':email', $user->email);
+                $request -> bindValue(':lastName', $user->lastName);
+                $request -> bindValue(':firstName', $user->firstName);
+                $request -> bindValue(':phone', $user->phone);
+                if ($request -> execute()){
+                    return ["error"=>false];
+                }else{
+                    throw new PDOException("user not add !");
+                }
             } else {
-                return 0;
+                $update = "UPDATE user SET lastName=:lastName, firstName=:firstName, phone=:phone WHERE email=:email";
+                $request = $pdo -> prepare($update);
+                $request -> bindValue(':email', $user->email);
+                $request -> bindValue(':lastName', $user->lastName);
+                $request -> bindValue(':firstName', $user->firstName);
+                $request -> bindValue(':phone', $user->phone);
+                if ($request -> execute()){
+                    return ["error"=>false];
+                }else{
+                    throw new PDOException("user not add !");
+                }
             }
         } catch (\Exception $e) {
             return(["error"=>$e->getMessage()]);
@@ -70,15 +68,15 @@ class UserManager extends Database
     }
 
     /**
-     * @param id $id id of user to be deleted in database
+     * @param string $email email of user to delete in database
      * @return array ["error" => false] on success | ["error" => message] on fail
      */
-    public static function deleteUserById($id){
+    public static function deleteUser($email){
         try{
             $pdo = self::connect();
-            $delete = "DELETE FROM user WHERE id = :id";
+            $delete = "DELETE FROM user WHERE email = :email";
             $request = $pdo -> prepare($delete);
-            $request -> bindValue(':id', $id);
+            $request -> bindValue(':email', $email);
             if ($request->execute()){
                 return ["error" => false];
             }else {
