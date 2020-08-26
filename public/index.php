41214ad75acdf3347de14a36f0e12decdb2ff723
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-26 16:57:00 +0200

    add altorouter

--- public/index.php
+++ public/index.php
@@ -2,13 +2,37 @@
 
 require_once '../vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('../application/template');
-$twig = new \Twig\Environment($loader, [
-    'cache' => false,
-]);
+//load twig function
+function loadTwig(){
+    $loader = new \Twig\Loader\FilesystemLoader('../application/template');
+    return new \Twig\Environment($loader, [
+        'cache' => '../application/cache',
+    ]);
+}
 
+// ALTOROUTER
+$router = new AltoRouter();
+$router->setBasePath('/public/');
 
-// Load template
+// index.php route
+$router->map('GET','*',function(){
+    $ads = \Ads\Manager\AdManager::getAllAds();
+    $categories = \Ads\Manager\CategoryManager::getAllCategories();
+    $users = \Ads\Manager\UserManager::getAllUsers();
+    $twig = loadTwig();
+    //load index template passing all Ad, Category, User objects
+    $template = $twig->load('index.html.twig');
+    echo $template->render(["ads"=>$ads,"categories"=>$categories,"users"=>$users]);
+});
 
-$template = $twig->load('index.html.twig');
-echo $template->render();
+// Match url
+$match = $router->match();
+var_dump($match);
+
+// Process request
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
