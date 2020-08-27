ef7b9f627ccbe3bb63b8b504aeb611244ef6d5f3
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-27 09:39:52 +0200

    add server constant

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 
 require_once '../vendor/autoload.php';
 
+//define application parameters
+define("BASE_PATH","");
+//passer BASE_PATH dans render pour l'ajouter au début des liens
+define("SERVER_NAME",$_SERVER["SERVER_NAME"]);
+//REQUEST_SCHEME SERVER_NAME BASE_PATH pour reconstruire les chemins
+
 //load twig function
 function loadTwig(){
     $loader = new \Twig\Loader\FilesystemLoader('../application/template');
@@ -12,10 +18,10 @@ function loadTwig(){
 
 // ALTOROUTER
 $router = new AltoRouter();
-$router->setBasePath('/public/');
+$router->setBasePath(BASE_PATH);
 
 // index.php route
-$router->map('GET','*',function(){
+$router->map('GET','/',function(){
     $ads = \Ads\Manager\AdManager::getAllAds();
     $categories = \Ads\Manager\CategoryManager::getAllCategories();
     $users = \Ads\Manager\UserManager::getAllUsers();
@@ -27,7 +33,6 @@ $router->map('GET','*',function(){
 
 // Match url
 $match = $router->match();
-var_dump($match);
 
 // Process request
 if( is_array($match) && is_callable( $match['target'] ) ) {
