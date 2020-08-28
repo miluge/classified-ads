760b7bdbffae9759e6bdcc2e182832db6a3447b6
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-28 14:20:39 +0200

    add isValidated method

--- application/class/Manager/AdManager.php
+++ application/class/Manager/AdManager.php
@@ -25,10 +25,27 @@ class AdManager extends Database
     }
 
     /**
+     * @param integer $id id of Ad to check validation
+     * @return boolean|array true if validated, false if not | ["error" => message] on fail
+     */
+    public static function isValidated($id){
+        try{
+            $pdo = self::connect();
+            $select = "SELECT validationDate FROM ad WHERE id=:id"; 
+            $request = $pdo -> prepare($select);
+            $request -> bindValue(':id', $id);
+            $request -> execute();
+            return boolval($request->fetch()["validationDate"]);
+        } catch (\Exception $e) {
+            return(["error"=>$e->getMessage()]);
+        }
+    }
+
+    /**
      * @param integer $id id of ad to select in database
      * @return Ad|array selected Ad instance on success | ["error" => message] on fail
      */
-    public static function getAd($id){
+    public static function get($id){
         try{
             $pdo = self::connect();
             $select = "SELECT ad.id, user_email, user.lastName AS user_lastName, user.firstName AS user_firstName, user.phone AS user_phone, category_id, category.name AS category_name, title, description, creationDate, validationDate, picture FROM ad INNER JOIN user ON user.email=ad.user_email INNER JOIN category ON category.id=ad.category_id WHERE ad.id = :id";
@@ -48,7 +65,7 @@ class AdManager extends Database
     /**
      * @return Ad[] array of all selected Ad instances on success | ["error" => message] on fail
      */
-    public static function getAllValidatedAds(){
+    public static function getAllValidated(){
         try{
             $pdo = self::connect();
             $select = "SELECT ad.id, user_email, user.lastName AS user_lastName, user.firstName AS user_firstName, user.phone AS user_phone, category_id, category.name AS category_name, title, description, validationDate, picture FROM ad INNER JOIN user ON user.email=ad.user_email INNER JOIN category ON category.id=ad.category_id WHERE validationDate IS NOT NULL";
@@ -71,10 +88,10 @@ class AdManager extends Database
      * if user have NO OTHER ADS, delete user from user table
      * @return array ["error" => false] on success | ["error" => message] on fail
      */
-    public static function deleteAd($id){
+    public static function delete($id){
         try{
             $pdo = self::connect();
-            $user_email = self::getAd($id)->user_email;
+            $user_email = self::get($id)->user_email;
             $delete = "DELETE FROM ad WHERE id = :id";
             $request = $pdo -> prepare($delete);
             $request -> bindValue(':id', $id);
@@ -95,7 +112,7 @@ class AdManager extends Database
      * @param Ad $ad Ad instance to insert in database
      * @return array ["error" => false] on success | ["error" => message] on fail
      */
-    public static function insertAd($ad){
+    public static function insert($ad){
         try{
             $pdo = self::connect();
             $insert = "INSERT INTO ad (user_email, category_id, title, description, creationDate, picture) VALUES (:user_email, :category_id, :title, :description, NOW(), :picture)";
@@ -119,7 +136,7 @@ class AdManager extends Database
      * @param Ad $ad Ad instance to update in database
      * @return array ["error" => false] on success | ["error" => message] on fail
      */
-    public static function updateAd($ad){
+    public static function update($ad){
         try{
             $pdo = self::connect();
             $update = "UPDATE ad SET category_id=:category_id, title=:title, description=:description, picture=:picture) WHERE id=:id";
@@ -140,15 +157,15 @@ class AdManager extends Database
     }
 
     /**
-     * @param Ad $ad Ad instance to validate in database
+     * @param integer $id of ad to validate in database
      * @return array ["error" => false] on success | ["error" => message] on fail
      */
-    public static function validateAd($ad){
+    public static function validate($id){
         try{
             $pdo = self::connect();
             $update = "UPDATE ad SET validationDate=NOW() WHERE id=:id";
             $request = $pdo -> prepare($update);
-            $request -> bindValue(':id', $ad->id);
+            $request -> bindValue(':id', $id);
             if ($request -> execute()){
                 return ["error" => false];
             } else {
