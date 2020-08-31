b45501f64ed2bdf6e2ef74ddc47119d20cca83df
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-31 10:43:51 +0200

    update check method

--- application/class/File.php
+++ application/class/File.php
@@ -23,19 +23,19 @@ class File
      * @return boolean|string true if file is ok | error message if not
      */
     public function check(){
-        $response = [];
         if (!in_array($this->extension, ["png", "jpg", "jpeg"])){
-            $response = "allowed file extension png, jpg, jpeg !";
+            return "allowed file extension png, jpg, jpeg !";
         }
+        $response = "";
         switch($this->error){
             case 0:
-                $response=true;
+                $response = true;
                 break;
             case 1: case 2:
-                $response="file is to large !";
+                $response = "file is to large !";
                 break;
             default:
-                $response="problem in file download !";
+                $response = "problem in file download !";
                 break;
         }
         return $response;
