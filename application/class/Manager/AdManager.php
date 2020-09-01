e0a3a17c81d20879129d615cb6d2ad84957dd412
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-01 15:54:31 +0200

    order validated ads by validation date

--- application/class/Manager/AdManager.php
+++ application/class/Manager/AdManager.php
@@ -80,7 +80,7 @@ class AdManager extends Database
     public static function getAllValidated(){
         try{
             $pdo = self::connect();
-            $select = "SELECT ad.id, user_email, user.lastName AS user_lastName, user.firstName AS user_firstName, user.phone AS user_phone, category_id, category.name AS category_name, title, description, validationDate, picture FROM ad INNER JOIN user ON user.email=ad.user_email INNER JOIN category ON category.id=ad.category_id WHERE validationDate IS NOT NULL";
+            $select = "SELECT ad.id, user_email, user.lastName AS user_lastName, user.firstName AS user_firstName, user.phone AS user_phone, category_id, category.name AS category_name, title, description, validationDate, picture FROM ad INNER JOIN user ON user.email=ad.user_email INNER JOIN category ON category.id=ad.category_id WHERE validationDate IS NOT NULL ORDER BY validationDate DESC";
             $request = $pdo -> prepare($select);
             $request -> execute();
             if ($ads = $request->fetchAll()) {
