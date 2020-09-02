10d47ac98c623037e6b9e9cb2bc27fdc25d81314
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-02 11:35:05 +0200

    update constructor and check() method

--- application/class/File.php
+++ application/class/File.php
@@ -9,34 +9,38 @@ class File
     public $error;
 
     /**
-     * @param array $array ["attribute"=>value] to hydrate attributes
-     * @return File newly created instance
+     * @param array $file array of $_FILES["file"]
      */
-    public function __construct($array) {
-        foreach($array as $property => $value){
-            $this->$property = $value;
-        }
-        return $this;
+    public function __construct($file) {
+        $this->name = basename($file["name"]);
+        $this->tmpName = $file["tmp_name"];
+        $this->error = $file["error"];
     }
 
     /**
      * @return boolean|string true if file is ok | error message if not
      */
     public function check(){
-        if (!in_array($this->extension, ["png", "jpg", "jpeg"])){
-            return "allowed file extension png, jpg, jpeg !";
-        }
         $response = "";
-        switch($this->error){
-            case 0:
-                $response = true;
-                break;
-            case 1: case 2:
-                $response = "file is to large !";
-                break;
-            default:
-                $response = "problem in file download !";
-                break;
+        if (empty($this->name)){// check file name
+            $response = "no file name provided";
+        }else{// check file extension
+            $this->extension = strtolower(pathinfo($this->name, PATHINFO_EXTENSION));
+            if (!in_array($this->extension, ["png", "jpg", "jpeg"])){
+                $response = "allowed file extension png, jpg, jpeg !";
+            }else{// check error status
+                switch($this->error){
+                    case 0:
+                        $response = true;
+                        break;
+                    case 1: case 2:
+                        $response = "file is to large !";
+                        break;
+                    default:
+                        $response = "problem in file upload !";
+                        break;
+                }
+            }
         }
         return $response;
     }
