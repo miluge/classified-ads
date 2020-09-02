1a68d7e63692551153969b4c180af0158e205c88
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-02 15:05:48 +0200

    update method to static

--- application/class/MJML.php
+++ application/class/MJML.php
@@ -7,7 +7,7 @@ abstract class MJML
      * @param string $mjml MJML formated string
      * @return string HTML formated string
      */
-    function getRender($mjml){
+    public static function getRender($mjml){
         $renderer = new \Qferrer\Mjml\Renderer\BinaryRenderer(dirname(dirname(dirname(__FILE__))).'/node_modules/.bin/mjml');
         return $renderer->render($mjml);
     }
