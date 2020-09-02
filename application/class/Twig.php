c18c1d6e5fd47dcbae53072342e74652e2ed2fed
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-02 12:07:57 +0200

    return render instead of echo render

--- application/class/Twig.php
+++ application/class/Twig.php
@@ -6,14 +6,15 @@ abstract class Twig
     /**
      * @param string $template template to load in render
      * @param array $params parameters to pass to template
-     * load Twig and echo $template render pasing $params
+     * load Twig and echo $template render passing $params
+     * @return string generated template render
      */
-    public static function echoRender($template, $params){
+    public static function getRender($template, $params){
         $loader = new \Twig\Loader\FilesystemLoader(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."template");
         $twig = new \Twig\Environment($loader, [
             'cache' => false,
             // 'cache' => dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."cache",
         ]);
-        echo $twig->render($template,$params);
+        return $twig->render($template,$params);
     }
 }
