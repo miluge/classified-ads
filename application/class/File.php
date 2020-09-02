781872e0d153571f251848aeca8afa47d91473d2
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-02 15:04:35 +0200

    format error message with _

--- application/class/File.php
+++ application/class/File.php
@@ -23,21 +23,21 @@ class File
     public function check(){
         $response = "";
         if (empty($this->name)){// check file name
-            $response = "no file name provided";
+            $response = "no_file_name_provided_!";
         }else{// check file extension
             $this->extension = strtolower(pathinfo($this->name, PATHINFO_EXTENSION));
             if (!in_array($this->extension, ["png", "jpg", "jpeg"])){
-                $response = "allowed file extension png, jpg, jpeg !";
+                $response = "allowed_file_extension_png,_jpg,_jpeg_!";
             }else{// check error status
                 switch($this->error){
                     case 0:
                         $response = true;
                         break;
                     case 1: case 2:
-                        $response = "file is to large !";
+                        $response = "file_is_to_large_!";
                         break;
                     default:
-                        $response = "problem in file upload !";
+                        $response = "problem_in_file_upload_!";
                         break;
                 }
             }
