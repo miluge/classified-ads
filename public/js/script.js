8829dd25293b7c95c9a070d52cea6f46a200cf2b
miluge <gllmblndl@gmail.com>
2020-09-03 08:44:18 +0200

    Title fix

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,9 @@
 
 $('.some-element').simpleLoadMore({
     item: '.element-item',
-    count: 9,
+    count: 3,
     // itemsToLoad: 10,
-    btnHTML: '<div><a href="#" class="load-more__btn bg-blue-500 ml-4 hover:bg-blue-400 text-white font-bold py-2 px-6 border-b-4 border-blue-700 hover:border-blue-500 rounded overflow-visible">View More <i class="fas fa-angle-down"></i></a></div>'
+    btnHTML: '<div class="mt-4"><a href="#" class="load-more__btn bg-blue-500 ml-4 hover:bg-blue-400 text-white font-bold py-2 px-6 border-b-4 border-blue-700 hover:border-blue-500 rounded overflow-visible">View More <i class="fas fa-angle-down"></i></a></div>'
   });
 
 /* Smooth Scroll to top */
