2cd3e2854f99602d0abf6911bfac2b96d9c6f6e1
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-07 14:44:27 +0200

    change method to deleteUserIfUseless()

--- application/class/Manager/AdManager.php
+++ application/class/Manager/AdManager.php
@@ -8,37 +8,28 @@ use \Ads\Manager\UserManager as UserManager;
 class AdManager extends Database
 {
     /**
-     * @return integer|boolean last inserted id on success | false on fail
+     * @param string $email User email to ckeck in database
+     * @return boolean true on success, false on fail
      */
-    public static function getLastId(){
-        try{
-            $pdo = self::connect();
-            return $pdo->lastInsertId();
-        } catch (\Exception $e) {
-            return(false);
-        }
-    }
-
-    /**
-     * @param string $email user_email to ckeck in database
-     * @return boolean|string true if exists, false if not| "error" on fail
-     */
-    public static function user_emailExists(string $email){
+    public static function deleteUserIfUseless(string $email){
         try{
             $pdo = self::connect();
             $select = "SELECT id FROM ad WHERE user_email=:user_email"; 
             $request = $pdo -> prepare($select);
             $request -> bindValue(':user_email', $email);
             $request -> execute();
-            return boolval($request->fetch());
-        } catch (\Exception $e) {
-            return("error");
+            if ($request->fetch()){
+                UserManager::delete($email);
+            }
+            return true;
+        } catch (\Exception $e){
+            return false;
         }
     }
 
     /**
-     * @param integer|string $id id of Ad to check validation
-     * @return boolean|string true if validated, false if not | "error" on fail
+     * @param integer $id id of Ad to check validation
+     * @return boolean true if Ad is validated, false if not or on fail
      */
     public static function isValidated($id){
         try{
@@ -49,12 +40,12 @@ class AdManager extends Database
             $request -> execute();
             return boolval($request->fetch()["validationDate"]);
         } catch (\Exception $e) {
-            return("error");
+            return false;
         }
     }
 
     /**
-     * @param integer|string $id id of ad to select in database
+     * @param integer $id id of ad to select in database
      * @return Ad|boolean selected Ad instance on success | false on fail
      */
     public static function get($id){
@@ -70,7 +61,7 @@ class AdManager extends Database
                 throw new \InvalidArgumentException("Ad not found !");
             }
         } catch (\Exception $e) {
-            return(false);
+            return false;
         }
     }
 
@@ -91,7 +82,7 @@ class AdManager extends Database
                 throw new \LengthException("No ads found !");
             }
         } catch (\Exception $e) {
-            return(false);
+            return false;
         }
     }
 
@@ -110,9 +101,7 @@ class AdManager extends Database
             $request -> bindValue(':id', $id);
             if ($request -> execute()){
                 //delete user if they have no other ad
-                if (self::user_emailExists($ad->user_email)===false){
-                    UserManager::delete($ad->user_email);
-                }
+                self::deleteUserIfUseless($ad->user_email);
                 //delete picture
                 if ($ad->picture!=="default.png" && file_exists(dirname(dirname(dirname(dirname(__FILE__))))."/public/assets/pictures/".$ad->picture)){
                     unlink(dirname(dirname(dirname(dirname(__FILE__))))."/public/assets/pictures/".$ad->picture);
@@ -122,7 +111,7 @@ class AdManager extends Database
                 throw new \InvalidArgumentException("Ad not found !");
             }
         } catch (\Exception $e) {
-            return(false);
+            return false;
         }
     }
 
@@ -146,7 +135,7 @@ class AdManager extends Database
                 throw new \PDOException("Ad not inserted !");
             }
         } catch (\Exception $e) {
-            return(false);
+            return false;
         }
     }
 
@@ -170,12 +159,12 @@ class AdManager extends Database
                 throw new \PDOException("Ad not updated !");
             }
         } catch (\Exception $e) {
-            return(false);
+            return false;
         }
     }
 
     /**
-     * @param integer|string $id of ad to validate in database
+     * @param integer $id of ad to validate in database
      * @return Ad|boolean validated Ad object on success | false on fail
      */
     public static function validate($id){
@@ -190,27 +179,7 @@ class AdManager extends Database
                 throw new \PDOException("Ad not validated !");
             }
         } catch (\Exception $e) {
-            return(false);
-        }
-    }
-
-    /**
-     * @param integer|string $id of ad to unvalidate in database
-     * @return Ad|boolean unValidated Ad object on success | false on fail
-     */
-    public static function unValidate($id){
-        try{
-            $pdo = self::connect();
-            $update = "UPDATE ad SET validationDate=NULL WHERE id=:id";
-            $request = $pdo -> prepare($update);
-            $request -> bindValue(':id', $id);
-            if ($request -> execute()){
-                return self::get($id);
-            } else {
-                throw new \PDOException("Ad not unValidated !");
-            }
-        } catch (\Exception $e) {
-            return(false);
+            return false;
         }
     }
 }
