a1b618cc18e6f6be81f5f581532028afa96d758c
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-07 14:45:42 +0200

    throw exceptions

--- application/class/Database.php
+++ application/class/Database.php
@@ -10,6 +10,7 @@ abstract class Database
 
     /**
      * @return PDO PDO instance of database
+     * @throws PDOException if connection fails
      */
     public static function connect(){
         return new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME . ';charset=utf8', self::USER, self::PASSWORD,[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
