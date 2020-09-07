f0476471be66a9d41e3eb37516dbac5c1e4f0a26
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-07 13:56:53 +0200

    move exception in child class

--- application/class/Database.php
+++ application/class/Database.php
@@ -12,10 +12,6 @@ abstract class Database
      * @return PDO|array PDO instance on success | ["error" => message] on fail
      */
     public static function connect(){
-        try{
-            return new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME . ';charset=utf8', self::USER, self::PASSWORD,[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
-        } catch (\PDOException $e){
-            return ["error" => $e->getMessage()];
-        }
+        return new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME . ';charset=utf8', self::USER, self::PASSWORD,[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
     }
 }
