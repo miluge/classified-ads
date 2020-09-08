15e00cfd3d1b518eed1a213a228b1a716d03a78d
miluge <gllmblndl@gmail.com>
2020-09-08 10:34:21 +0200

    PDF download working

--- public/js/downloadPDF.js
+++ public/js/downloadPDF.js
@@ -1,13 +1,5 @@
-var content = document.getElementById('description'),
-button = document.getElementById('print');
 
-function generatePDF(){
-  var doc = new jsPDF();
-
-  doc.setFontSize(14);
-  doc.text(25, 25, "Paranyan loves jsPDF");
-//   doc.text(20, 20, content);
-  doc.save('my.pdf');
+function downloadPDF() {
+    var element = document.getElementById('card');
+    html2pdf(element);
 }
-
-button.addEventListener('click', generatePDF);
