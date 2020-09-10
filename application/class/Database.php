ab77cbef3f9eee41bbd4f863fb7375d735eda202
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-10 14:49:46 +0200

    use parameters for database connection

--- application/class/Database.php
+++ application/class/Database.php
@@ -3,16 +3,11 @@ namespace Ads;
 
 abstract class Database
 {
-    const HOST = "localhost";
-    const USER = "root";
-    const PASSWORD = "";
-    const NAME = "classified-ads";
-
     /**
      * @return PDO PDO instance of database
      * @throws PDOException if connection fails
      */
     public static function connect(){
-        return new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME . ';charset=utf8', self::USER, self::PASSWORD,[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
+        return new \PDO('mysql:host=' . HOST . ';dbname=' . NAME . ';charset=utf8', USER, PASSWORD,[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
     }
 }
